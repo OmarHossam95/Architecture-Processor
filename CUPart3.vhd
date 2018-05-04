ba0e7571165d7460107acc457d4a1c353a5bdded
Mohammedibra96 <mohammedibra96@gmail.com>
2018-05-04 19:36:15 +0200

    Add files via upload

---
+++ CUPart3.vhd
@@ -0,0 +1,24 @@
+Library ieee;
+Use ieee.std_logic_1164.all;
+
+
+entity CUPart3 is 
+
+port( 
+OPGroup: in std_logic_vector(1 downto 0);
+OP:in std_logic_vector(4 downto 0);
+
+Stall,INTMWB:in std_logic;
+INTBegin,JumpEn,RTN:out std_logic
+
+);
+end entity;
+
+Architecture CUImp of CUPart3 is
+signal OutAnd :std_logic;
+begin
+OutAnd<=OPGroup(0) and OPGroup(1);
+RTN<=OP(3) and OutAnd;
+INTBegin<=INTMWB;
+JumpEn<=Stall;
+end CUImp;
